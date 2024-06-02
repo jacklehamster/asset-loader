d37e5d045ff7fd0c698429163c549050c22b6de1
Jack Le Hamster <jacklehamster@gmail.com>
2024-06-01 18:35:05 -0700

    Switch to webpack.

---
+++ webpack.config.cjs
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production', // or 'development' for development mode
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
