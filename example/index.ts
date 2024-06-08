2a540ffcc3be11a25e71b775fe4d535e99127b26
Jack Le Hamster <jacklehamster@gmail.com>
2024-06-08 10:01:29 -0700

    Move website to docs folder.

--- example/index.ts
+++ example/index.ts
@@ -3,7 +3,7 @@ import serveStatic from "serve-static-bun";
 
 const app = new Bao();
 
-app.get("/*any", serveStatic("/dist", { middlewareMode: "bao" }));
+app.get("/*any", serveStatic("/../docs", { middlewareMode: "bao" }));
 
 const server = app.listen({ port: 3000 });
 console.log(`Listening on http://localhost:${server.port}`);
